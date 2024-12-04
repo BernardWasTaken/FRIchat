e4727c92d52e91af7e30c70c17bbff930540fbe7
Luka <luka.bozic2004@gmail.com>
2024-12-04 21:48:56 +0100

    added frontend

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,41 +1,48 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using FRIchat.Models;
+using FRIchat.Data;  // Dodajte ta prostor imen za dostop do vašega DbContext
 
-namespace FRIchat.Controllers;
-
-public class HomeController : Controller
+namespace FRIchat.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly FRIchatContext _context;  // Dodajte kontekst
 
-    public HomeController(ILogger<HomeController> logger)
-    {
-        _logger = logger;
-    }
+        // Konstruktor, ki sprejme DbContext
+        public HomeController(ILogger<HomeController> logger, FRIchatContext context)
+        {
+            _logger = logger;
+            _context = context;  // Inicializirajte kontekst
+        }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        // Akcija Index, ki vrne seznam predmetov
+        public IActionResult Index()
+        {
+            var predmeti = _context.Predmet.ToList();  // Pridobite seznam predmetov iz baze
+            return View(predmeti);  // Pošljite seznam predmetov v pogled
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        public IActionResult About()
+        {
+            return View();
+        }
 
-    public IActionResult Register()
-    {
-        return Redirect($"/Uporabnik/Create");
-    }
-    
-    public IActionResult Login()
-    {
-        return Redirect($"/Uporabnik/Login");
-    }
+        public IActionResult Register()
+        {
+            return Redirect($"/Uporabnik/Create");
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        public IActionResult Login()
+        {
+            return Redirect($"/Uporabnik/Login");
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
 }
